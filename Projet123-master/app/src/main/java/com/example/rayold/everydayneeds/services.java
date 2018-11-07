634cc04c1154a9c0056946abe583f96a36b0f4bd
Amadou Diagne <adiag026@uottawa.ca>
2018-11-07 16:30:46 -0500

    second commit

---
+++ Projet123-master/app/src/main/java/com/example/rayold/everydayneeds/services.java
@@ -0,0 +1,42 @@
+/*package com.example.rayold.everydayneeds;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class services extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_services);
+
+        // Get ListView object from xml layout
+        ListView listView = (ListView) findViewById(R.id.list);
+//Defining Array values to show in ListView
+        String[] values = new String[] {
+                "Item 01","Item 02","Item 03","Item 04","Item 05","Item 06","Item 07","Item 08"
+        };
+//Converting Array to ArrayList
+        final ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < values.length; ++i) {
+            list.add(values[i]);
+        }
+//Create an ArrayAdapter and Set it on the ListView
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
+                final String item = (String) parent.getItemAtPosition(position);
+                //Do something with the string that you just got!
+            }
+        });
+    }
+}
+*/
